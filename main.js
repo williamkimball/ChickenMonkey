e9aa8c2fa438150c0c1549a687d5ed19ac300d2a
William Kimball <wkkimball043@gmail.com>
2018-05-23 09:28:14 -0500

    wrote battle of bands excercise

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 
-//ChickenMonkey
-for (i = 1; i <=100; i++) {
-    if (i % 5 === 0 && i % 7 ===0) { //check if i is a multiple of 5 and 7
-        console.log("ChickenMonkey")
-    } else if (i % 5 === 0) {  //check if i is a multiple of 5
-        console.log("Chicken")
-    } else if (i % 7 === 0) {  //check if i is a multiple of 7
-        console.log("Monkey")
-    } else {                   //if nothing else is true, console.log the number
-        console.log(i)
+function chickenMonkey () {
+    for (i = 1; i <=100; i++) {
+            if (i % 5 === 0 && i % 7 ===0) { //check if i is a multiple of 5 and 7
+                console.log("ChickenMonkey")
+            } else if (i % 5 === 0) {  //check if i is a multiple of 5
+                console.log("Chicken")
+            } else if (i % 7 === 0) {  //check if i is a multiple of 7
+                console.log("Monkey")
+            } else {                   //if nothing else is true, console.log the number
+                console.log(i)
+            }
+        }
     }
+    
+chickenMonkey()
+
+let bandNum = 1;
+
+function takeNum (bandName) {
+    console.log(`${bandNum}. ${bandName} `);
+    bandNum++;
 }
+takeNum('Panic!')
+takeNum('Jason Lancaster')
