9e78723e5c258797d27cccb720ab2e6bfa135a2e
William Kimball <wkkimball043@gmail.com>
2018-05-23 09:00:29 -0500

    wrote for loop with functions to print 1-100 with specific messages for multiples of 5 and 7

---
+++ main.js
@@ -0,0 +1,13 @@
+for (i = 1; i <=100; i++) {
+    if (i % 5 === 0 && i % 7 ===0) {
+        console.log("ChickenMonkey")
+    }
+    else if (i % 5 === 0) {
+        console.log("Chicken")
+    } else if (i % 7 === 0) {
+        console.log("Monkey")
+    }
+    else {
+        console.log(i)
+    }
+}
