6a1556f8055d9f6e0f7020648c66a742665cfff4
William Kimball <wkkimball043@gmail.com>
2018-05-23 09:01:22 -0500

    cleaned up code

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 for (i = 1; i <=100; i++) {
     if (i % 5 === 0 && i % 7 ===0) {
         console.log("ChickenMonkey")
-    }
-    else if (i % 5 === 0) {
+    } else if (i % 5 === 0) {
         console.log("Chicken")
     } else if (i % 7 === 0) {
         console.log("Monkey")
-    }
-    else {
+    } else {
         console.log(i)
     }
 }
